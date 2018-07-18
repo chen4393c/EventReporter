1ce946444615f258345e0735d843bf651714a6d4
chen4393c <chen4393c@gmail.com>
2018-07-18 17:08:06 -0500

    Set Up FCM

---
+++ app/src/main/java/com/laioffer/eventreporter/FCM/MyFirebaseMessagingService.java
@@ -0,0 +1,42 @@
+package com.laioffer.eventreporter.FCM;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+
+    // Called when message is received
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                //scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+
+    // Handle time allotted to BroadcastReceivers
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+}

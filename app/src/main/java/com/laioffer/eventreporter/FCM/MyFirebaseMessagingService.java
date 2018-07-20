b98d76e8a6d650a81af0b763ac516c936688998f
chen4393c <chen4393c@gmail.com>
2018-07-20 01:56:03 -0500

    Android Notification

--- app/src/main/java/com/laioffer/eventreporter/FCM/MyFirebaseMessagingService.java
+++ app/src/main/java/com/laioffer/eventreporter/FCM/MyFirebaseMessagingService.java
@@ -1,9 +1,18 @@
 package com.laioffer.eventreporter.FCM;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.laioffer.eventreporter.MainActivity;
+import com.laioffer.eventreporter.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -25,8 +34,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 // Handle message within 10 seconds
                 handleNow();
             }
-
         }
+
+        sendNotification("Send notification to start EventReporter");
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
@@ -38,5 +48,41 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     // Handle time allotted to BroadcastReceivers
     private void handleNow() {
         Log.d(TAG, "Short lived task is done.");
+    }
+
+    /*
+    * Create and show a simple notification containing the received FCM message
+    * */
+    private void sendNotification(String fcmMessage) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Define pending intent to trigger activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        // Create Notification according to builder pattern
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, "EventReport");
+        notificationBuilder
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle("FCM Message")
+                .setContentText(fcmMessage)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        // Get Notification Manager
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Send notification
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
